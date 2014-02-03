c6b5257b3d867123ff16e6100e9dd660374232db
Alexander Selzer <aselzer3@gmail.com>
2014-02-03 19:03:04 +0100

    Yay! Gulp.

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require("gulp");
+var log = require("gulp-util").log;
+var jshint = require("gulp-jshint");
+var mocha = require("gulp-mocha");
+
+gulp.task("jshint", function() {
+  log("jshinting files.");
+  gulp.src("./*.js")
+  .pipe(jshint());
+});
+
+gulp.task("mocha", function() {
+  gulp.src("./tests/*.js")
+  .pipe(mocha({reporter: "nyan"}));
+});
+
+gulp.task("watch", function() {
+  log("Watching...");
+  gulp.watch(paths, ["jshint"]);
+});
+
+gulp.task("default", ["jshint", "mocha"]);
--- tests/test.js
+++ tests/test.js
@@ -8,7 +8,6 @@ describe("Citation#getMlaReference", function() {
     citation.getMlaReference(function(err, reference) {
       if (err) throw err;
       done();
-      console.log(reference)
     });
   });
 });
@@ -19,20 +18,28 @@ describe("Citation#getReference", function() {
     citation.getReference(function(err, reference) {
       if (err) throw err;
       done();
-      console.log(reference)
+      describe("Citation#convertToMla", function() {
+        it("should convert the citation to MLA from JSON", function(done) {
+          var jsonReference = citation.convertToMla(reference);
+          done();
+        });
+      });
     });
   });
 });
 
-describe("Citation#getMlaReference", function() {
-  this.timeout(20000);
-  it("should get a HTTPS MLA citation for the MDN JS RegExp page.", function(done) {
+describe("Citation#changeSite", function() {
+  it("should change the site of the citation object.", function(done) {
     citation.setSite("https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp");
-    citation.getMlaReference(function(err, reference) {
-      if (err) throw err;
-      done();
-      console.log(reference);
+    done();
+    describe("Citation#getMlaReference", function() {
+      this.timeout(20000);
+      it("should get a HTTPS MLA citation for the MDN JS RegExp page.", function(done) {
+        citation.getMlaReference(function(err, reference) {
+          if (err) throw err;
+          done();
+        });
+      });
     });
   });
 });
-
