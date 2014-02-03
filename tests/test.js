38679c21e9058d9cf1579894a19b69200590e1d8
Alexander Selzer <aselzer3@gmail.com>
2014-02-03 16:14:16 +0100

    Added HTTPS support.

--- tests/test.js
+++ tests/test.js
@@ -3,7 +3,8 @@ var Citation = require("../citation.js");
 var citation = new Citation("http://en.wikipedia.org/wiki/JavaScript");
 
 describe("Citation#getMlaReference", function() {
-  it("should get a citation for the JavaScript Wikipedia page.", function(done) {
+  this.timeout(20000);
+  it("should get a JSON citation for the JavaScript Wikipedia page.", function(done) {
     citation.getMlaReference(function(err, reference) {
       if (err) throw err;
       done();
@@ -11,3 +12,27 @@ describe("Citation#getMlaReference", function() {
     });
   });
 });
+
+describe("Citation#getReference", function() {
+  this.timeout(20000);
+  it("should get a JSON citation for the JavaScript Wikipedia page.", function(done) {
+    citation.getReference(function(err, reference) {
+      if (err) throw err;
+      done();
+      console.log(reference)
+    });
+  });
+});
+
+describe("Citation#getMlaReference", function() {
+  this.timeout(20000);
+  it("should get a HTTPS MLA citation for the MDN JS RegExp page.", function(done) {
+    citation.setSite("https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp");
+    citation.getMlaReference(function(err, reference) {
+      if (err) throw err;
+      done();
+      console.log(reference);
+    });
+  });
+});
+
